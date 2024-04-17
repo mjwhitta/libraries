505e74cf98b75ee0ace3a0a83fa5d4ac8bbb6433
Miles Whittaker <mj@whitta.dev>
2024-04-17 15:27:21 -0400

    Missing constants, Go Docs, configuration support
    - Added missing constants from
      https://docs.preludesecurity.com/docs/understanding-results
    - Added map of exit code to human-readable message for logging
    - Go Docs
    - Start() now supports configuration via the global Cfg var
    - Start() now calls Stop() on timeout
        - No fancy sleep method
        - Now logs when timeout occurs
    - Stop() now uses mutex to ensure single execution
    - Stop() now traverses up the stack until no longer in endpoint.go
        - Ensures log file contains line number for custom test rather
          than module
    - Stop() now logs exit code along with human-readable message
    - Due to Start()/Stop() changes there is one side-effect that I'm
      aware of
        - cleanup() is called exactly once, but test() continues running
          b/c it's a goroutine
    - Fixed Wait() to sleep for the correct duration
        - Before, if duration was > 0, e.g. "x * time.Second", it would
          sleep for "x * time.Second * time.Second" which is "x * 10^18"
        - This might break existing tests if they were doing something
          like "Endpoint.Wait(5)", but it doesn't look like any default
          tests use Wait()
    - Added Sayf() to remove need for Say(fmt.Sprintf(...))
    - Find() feels too vague so I renamed to FindByType() and added Find()
      as a wrapper, documented as deprecated
    - FindByType() accepts optional paths, defaulting to the user's home
      directory as before
    - Added log messages to FindByType()
    - Read() now supports configuration via the global Cfg var
    - Added and clarified log messages in Read() and Write()
    - Write() now supports configuration via the global Cfg var
    - Write() now creates parent folder for provided file
    - Exists() now logs error, if the path is inaccessible (permissions)
    - Added IsAccessible() to determine if a file is accessible or not
    - GetOS() now uses switch
    - Quarantined() now supports configuration via the global Cfg var
    - Added log message to Remove()
    - Added RemoveAll()
    - Added Run() which will run a command in the background and return
      the process handle
        - You can kill that process later, but grandchild process will
          hang around (Go limitation)
    - Added log message to Shell()
    - Shell() now uses switch for type checking returned error
    - Run() and Shell() now supports configuration via the global Cfg var
    - Pwd() now takes optional list of filenames
    - generateKey() now takes a size rather than a hard-coded 32
    - Unzip wrote to "./" which may not be expected
        - Now defaults to Pwd() to mirror Read() and Write()
        - Added path as an optional argument
        - Fixed extraction for zip files that only contain file entries
    - Added log messages to Unzip()
    - Replaced hard-coded Stop() codes with defined constants
    - Used existing functions where possible
        - IsAccessible() vs os.Open()
        - Sayf() vs Say(fmt.Sprintf(...))
        - Stop() vs os.Exit()
        - Wait() vs time.Sleep()
    - Removed some unused local variables
    - Added Config type for configuration of directory, noEscape, timeout,
      and future options

--- go/tests/endpoint/endpoint.go
+++ go/tests/endpoint/endpoint.go
@@ -14,21 +14,28 @@ import (
 	"path/filepath"
 	"runtime"
 	"strings"
+	"sync"
 	"time"
 )
 
+// Return codes as defined by:
+// https://docs.preludesecurity.com/docs/understanding-results
 const (
 	// Errors
 	UnexpectedTestError      int = 1
+	MalformedTestError       int = 2
 	TimeoutExceeded          int = 102
 	CleanupFailed            int = 103
 	OutOfMemory              int = 137
 	UnexpectedExecutionError int = 256
 
 	// Not Relevant
-	NotRelevant int = 104
+	NotRelevant   int = 104
+	NotRelevantOS int = 108
 
 	// Protected
+	TestForceKilled             int = 9
+	TestGracefullyKilled        int = 15
 	TestCompletedNormally       int = 100
 	FileQuarantinedOnExtraction int = 105
 	NetworkConnectionBlocked    int = 106
@@ -41,50 +48,97 @@ const (
 	TestIncorrectlyBlocked int = 110
 )
 
-type fn func()
+var cleanup func() = func() {}
 
-var cleanup fn = func() {}
+var results = map[int]string{
+	CleanupFailed:               "cleanup failed",
+	ExecutionPrevented:          "execution prevented",
+	FileQuarantinedOnExecution:  "file quarantined on execution",
+	FileQuarantinedOnExtraction: "file quarantined on extraction",
+	HostNotVulnerabile:          "host not vulnerable",
+	MalformedTestError:          "malformed test error",
+	NetworkConnectionBlocked:    "network connection blocked",
+	NotRelevant:                 "not relevant",
+	NotRelevantOS:               "not relevant OS",
+	OutOfMemory:                 "out of memory",
+	TestCompletedNormally:       "test completed normally",
+	TestForceKilled:             "test force killed",
+	TestGracefullyKilled:        "test gracefully killed",
+	TestIncorrectlyBlocked:      "test incorrectly blocked",
+	TimeoutExceeded:             "timeout exceeded",
+	UnexpectedExecutionError:    "unexpected execution error",
+	UnexpectedTestError:         "unexpected test error",
+	Unprotected:                 "unprotected",
+}
 
-func Start(test fn, clean ...fn) {
+var stopMutex *sync.Mutex = &sync.Mutex{}
+
+// Start takes a test function and runs it in the background. By
+// default it waits 30 seconds before timeout. The cleanup function is
+// run when Stop() is called. The default cleanup does nothing. A
+// custom clean function can be optionally provided. The timeout can
+// be overridden by configuring Cfg in advance.
+func Start(test func(), clean ...func()) {
+	var cfg Config = defaultCfg(Config{timeout: 30 * time.Second})
+
 	if len(clean) > 0 {
 		cleanup = clean[0]
 	}
 
-	Say(fmt.Sprintf("Starting test at: %s", time.Now().Format("2006-01-02T15:04:05")))
+	Say("Starting test")
 
 	go func() {
 		test()
 	}()
 
-	select {
-	case <-time.After(30 * time.Second):
-		os.Exit(102)
-	}
+	time.Sleep(cfg.timeout)
+	Sayf("Test execution exceeded time limit: %s", cfg.timeout)
+	Stop(TimeoutExceeded)
 }
 
+// Stop will call the associated cleanup function (provided when
+// Start() was called) and then exit with the provided code.
 func Stop(code int) {
-	cleanup()
-	// Get the caller's line number
-	_, _, line, _ := runtime.Caller(1)
+	// Only allow one stop
+	stopMutex.Lock()
+	defer stopMutex.Unlock()
 
-	Say(fmt.Sprintf("Completed with code: %d", code))
-	Say(fmt.Sprintf("Exit called from line: %d", line))
-	Say(fmt.Sprintf("Ending test at: %s", time.Now().Format("2006-01-02T15:04:05")))
+	cleanup()
+
+	// Get the caller's line number (might have to go up levels if
+	// Stop() is used by the module itself)
+	caller := 1
+	_, fn, line, _ := runtime.Caller(caller)
+	for filepath.Base(fn) == "endpoint.go" {
+		caller++
+		_, fn, line, _ = runtime.Caller(caller)
+	}
+
+	// Get user-friendly message for the provided code
+	result, ok := results[code]
+	if !ok {
+		result = "undefined"
+	}
+
+	Sayf("Completed with code: %d (%s)", code, result)
+	Sayf("Exit called from line: %d", line)
+	Say("Ending test")
 
 	os.Exit(code)
 }
 
-// NB: time.Duration is an int64 cast
-func Wait(dur time.Duration) {
-	if dur <= 0 { // default
-		Say("Waiting for 3 seconds")
-		time.Sleep(3 * time.Second)
-	} else {
-		Say(fmt.Sprintf("Waiting for %d seconds", dur))
-		time.Sleep(dur * time.Second)
+// Wait will sleep for the specified duration. If duration is <= 0, it
+// will default to 3 seconds.
+func Wait(dur ...time.Duration) {
+	if (len(dur) == 0) || (dur[0] <= 0) { // default
+		dur = []time.Duration{3 * time.Second}
 	}
+	Sayf("Waiting for %s", dur[0].String())
+	time.Sleep(dur[0])
 }
 
+// Say will print a message prepended with a timestamp and the
+// file/test name.
 func Say(print string) {
 	filename := filepath.Base(os.Args[0])
 	name := strings.TrimSuffix(filename, filepath.Ext(filename))
@@ -92,87 +146,212 @@ func Say(print string) {
 	fmt.Printf("[%s][%s] %v\n", timeStamp, name, print)
 }
 
+// Sayf will print a formatted message prepended with a timestamp and
+// the file/test name.
+func Sayf(print string, args ...any) {
+	Say(fmt.Sprintf(print, args...))
+}
+
+// Find is deprecated. use FindByType().
 func Find(ext string) []string {
-	dirname, _ := os.UserHomeDir()
+	return FindByType(ext)
+}
+
+// FindByType will walk the provided paths looking for files that have
+// the provided file extension. If no paths are provided, it defaults
+// to the user's home directory.
+func FindByType(ext string, paths ...string) []string {
 	var a []string
-	filepath.WalkDir(dirname, func(s string, d fs.DirEntry, e error) error {
-		if e == nil {
-			if filepath.Ext(d.Name()) == ext {
-				Say(fmt.Sprintf("Found: %s", s))
-				a = append(a, s)
-			}
+	if len(paths) == 0 {
+		if home, e := os.UserHomeDir(); e != nil {
+			Sayf("Unable to determine home directory: %s", e)
+		} else {
+			paths = []string{home}
 		}
-		return nil
-	})
+	}
+	Sayf("Searching for %s files...", ext)
+	for _, path := range paths {
+		filepath.WalkDir(
+			path,
+			func(s string, d fs.DirEntry, e error) error {
+				if e == nil {
+					if filepath.Ext(d.Name()) == ext {
+						Sayf("Found: %s", s)
+						a = append(a, s)
+					}
+				}
+				return nil
+			},
+		)
+	}
+	Sayf("Found %d files", len(a))
 	return a
 }
 
-func Read(path string) []byte {
-	bit, err := os.ReadFile(Pwd(path))
+// Read will read a file and return the contents. An empty slice means
+// failed read. By default the filename is assumed to be in Pwd(). You
+// can override this behavior by configuring Cfg in advance.
+func Read(filename string) []byte {
+	var cfg Config = defaultCfg(Config{directory: Pwd()})
+	var path string = filepath.Join(cfg.directory, filename)
+	Sayf("Reading %s...", path)
+	b, err := os.ReadFile(path)
 	if err != nil {
-		Say(fmt.Sprintf("Error: %s", err))
+		Sayf("Failed to read %s: %s", path, err)
+		return nil
 	}
-	return bit
+	Sayf("Successfully read %d bytes from %s", len(b), path)
+	return b
 }
 
-func Write(filename string, contents []byte) {
-	err := os.WriteFile(Pwd(filename), contents, 0644)
-	if err != nil {
-		Say("Failed to write " + filename)
-	}
-}
-
-func Exists(path string) bool {
-	if _, err := os.Stat(path); err == nil {
-		return true
-	} else {
+// Write will write the provided contents to the provided file path
+// and return any errors. By default the filename is assumed to be in
+// Pwd(). You can override this behavior by configuring Cfg in
+// advance.
+func Write(filename string, contents []byte) bool {
+	var cfg Config = defaultCfg(Config{directory: Pwd()})
+	var path string = filepath.Join(cfg.directory, filename)
+	Sayf("Writing to %s...", path)
+	parent := filepath.Dir(path)
+	if err := os.MkdirAll(parent, 0o755); err != nil {
+		Sayf("Failed to create parent folder %s: %s", parent, err)
 		return false
 	}
-}
-
-func GetOS() string {
-	os := runtime.GOOS
-
-	if os != "windows" && os != "linux" && os != "darwin" {
-		return "unsupported"
-	}
-
-	return os
-}
-
-func Quarantined(filename string, contents []byte) bool {
-	Write(filename, contents)
-	path := Pwd(filename)
-	time.Sleep(3 * time.Second)
-	if Exists(path) {
-		file, err := os.Open(path)
-		if err != nil {
-			return true
-		}
-		defer file.Close()
+	if err := os.WriteFile(path, contents, 0o644); err != nil {
+		Sayf("Failed to write %s: %s", path, err)
 		return false
 	}
+	Sayf("Successfully wrote %d bytes to %s", len(contents), path)
 	return true
 }
 
-func Remove(path string) bool {
-	e := os.Remove(path)
-	return e == nil
+// Exists checks if a file exists AND can be accessed. If this
+// function returns false, the file might still exist, but the current
+// user does not have the required privileges to access it. Check the
+// log for more details.
+func Exists(path string) bool {
+	if _, err := os.Stat(path); err == nil {
+		return true
+	} else if os.IsNotExist(err) {
+		return false
+	} else {
+		err = err.(*fs.PathError).Err
+		Sayf("Path %s not accessible: %s", path, err)
+		return false
+	}
 }
 
-func Shell(args []string) (string, error) {
+// IsAccessible will return whether or not the provided path can be
+// opened.
+func IsAccessible(path string) bool {
+	f, err := os.Open(path)
+	if err != nil {
+		return false
+	}
+	f.Close()
+	return true
+}
+
+// GetOS returns the runtime OS, or "unsupported", if not supported.
+func GetOS() string {
+	switch runtime.GOOS {
+	case "darwin", "linux", "windows":
+		return runtime.GOOS
+	default:
+		return "unsupported"
+	}
+}
+
+// Quarantined will write the provided bytes to the proviled filename
+// in the same directory as the test executable. It then waits and
+// checks to see if the file exists. If the file is not found or is
+// inaccessible, it is assumed to have been quarantined. The default
+// directory is Pwd(), but can be overridden by configuring Cfg in
+// advance. The default wait time is 3 seconds, but can be overridden
+// by configuring Cfg in advance.
+func Quarantined(filename string, contents []byte) bool {
+	var cfg Config = defaultCfg(
+		Config{directory: Pwd(), timeout: 3 * time.Second},
+	)
+	var path string = filepath.Join(cfg.directory, filename)
+	// Use the local config for nested Write()
+	Cfg = &cfg
+	// Do not use full path here b/c Write() will handle that
+	Write(filename, contents)
+	Wait(cfg.timeout)
+	Say("Checking for quarantine")
+	if Exists(path) && IsAccessible(path) {
+		Say("Not quarantined")
+		return false
+	}
+	Say("Successfully quarantined")
+	return true
+}
+
+// Remove will attempt to remove a file and returns true upon success.
+// See the log for any errors.
+func Remove(path string) bool {
+	if err := os.Remove(path); err != nil {
+		Sayf("Failed to remove %s: %s", path, err)
+		return false
+	}
+	Sayf("Successfully removed %s", path)
+	return true
+}
+
+// RemoveAll will attempt to remove a directory and returns true upon
+// success. See the log for any errors.
+func RemoveAll(path string) bool {
+	if err := os.RemoveAll(path); err != nil {
+		Sayf("Failed to recursively remove %s: %s", path, err)
+		return false
+	}
+	Sayf("Successfully removed %s", path)
+	return true
+}
+
+// Run will attempt to run the provided command and args as a new
+// process. It returns the new process handle and any error that
+// occurs. The caller should decide whether to call Kill() or Wait()
+// on the returned process handle.
+func Run(args []string) (*os.Process, error) {
+	var cfg Config = defaultCfg(Config{})
+	Sayf("Running \"%s\" in the background", strings.Join(args, " "))
 	cmd := exec.Command(args[0], args[1:]...)
+	if attrs := procAttrs(args); cfg.noEscape && (attrs != nil) {
+		cmd.SysProcAttr = attrs
+	}
+	if err := cmd.Start(); err != nil {
+		err = fmt.Errorf("failed to run cmd: %w", err)
+		return nil, err
+	}
+	return cmd.Process, nil
+}
+
+// Shell will attempt to run the provided command and args as a new
+// process. It returns the STDOUT or an error containing the STDERR.
+func Shell(args []string) (string, error) {
+	var cfg Config = defaultCfg(Config{})
+	Sayf("Running \"%s\"", strings.Join(args, " "))
+	cmd := exec.Command(args[0], args[1:]...)
+	if attrs := procAttrs(args); cfg.noEscape && (attrs != nil) {
+		cmd.SysProcAttr = attrs
+	}
 	stdout, err := cmd.Output()
 	if err != nil {
-		if exitError, ok := err.(*exec.ExitError); ok {
-			return "", fmt.Errorf("%s: %s", err.Error(), string(exitError.Stderr))
-		} else {
+		switch err := err.(type) {
+		case *exec.ExitError:
+			return "", fmt.Errorf("%s", string(err.Stderr))
+		default:
+			err = fmt.Errorf("failed to read cmd output: %w", err)
 			return "", err
 		}
 	}
 	return string(stdout), nil
 }
 
+// ExecuteRandomCommand will choose a random command from the provided
+// list and execute it with Shell().
 func ExecuteRandomCommand(commands [][]string) (string, error) {
 	var command []string
 	if len(commands) == 0 {
@@ -183,43 +362,54 @@ func ExecuteRandomCommand(commands [][]string) (string, error) {
 		index := rand.Intn(len(commands))
 		command = commands[index]
 	}
-
 	return Shell(command)
 }
 
+// IsAvailable will look for a list of tools and check to see if any
+// on in the system's PATH. It returns true upon the first tool found,
+// false if none are found.
 func IsAvailable(programs ...string) bool {
 	for _, program := range programs {
-		_, err := exec.LookPath(program)
-		if err == nil {
+		if _, err := exec.LookPath(program); err == nil {
 			return true
 		}
 	}
 	return false
 }
 
-func Pwd(filename string) string {
+// Pwd will return the directory where the test is located on disk. It
+// is important to note that this may not be the directory from which
+// the test is running.
+func Pwd(filename ...string) string {
 	bin, err := os.Executable()
 	if err != nil {
 		Say("Failed to get path")
-		Stop(256)
+		Stop(UnexpectedExecutionError)
 	}
-	filePath := filepath.Join(filepath.Dir(bin), filename)
-	return filePath
+	if len(filename) == 0 {
+		return filepath.Dir(bin)
+	}
+	return filepath.Join(
+		append([]string{filepath.Dir(bin)}, filename...)...,
+	)
 }
 
+// XorEncrypt will use xor to encrypt data with a randomly
+// generated key. It returns the encrypted data with the key.
 func XorEncrypt(data []byte) ([]byte, []byte, error) {
-	keyData, err := generateKey()
+	key, err := generateKey(32)
 	if err != nil {
 		return nil, nil, err
 	}
 
 	encrypted := make([]byte, len(data))
 	for i, v := range data {
-		encrypted[i] = v ^ (keyData[i%len(keyData)] + byte(i))
+		encrypted[i] = v ^ (key[i%len(key)] + byte(i))
 	}
-	return encrypted, keyData, nil
+	return encrypted, key, nil
 }
 
+// XorDecrypt will use xor to decrypt data with the provided key.
 func XorDecrypt(data []byte, key []byte) []byte {
 	decrypted := make([]byte, len(data))
 	for i, v := range data {
@@ -228,8 +418,10 @@ func XorDecrypt(data []byte, key []byte) []byte {
 	return decrypted
 }
 
+// AES256GCMEncrypt will use AES256GCM to encrypt data with a randomly
+// generated key. It returns the encrypted data with the key.
 func AES256GCMEncrypt(data []byte) ([]byte, []byte, error) {
-	key, err := generateKey()
+	key, err := generateKey(32)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -253,6 +445,8 @@ func AES256GCMEncrypt(data []byte) ([]byte, []byte, error) {
 	return ciphertext, key, nil
 }
 
+// AES256GCMDecrypt will use AES256GCM to decrypt data with the
+// provided key.
 func AES256GCMDecrypt(data, key []byte) ([]byte, error) {
 	blockCipher, err := aes.NewCipher(key)
 	if err != nil {
@@ -277,27 +471,49 @@ func AES256GCMDecrypt(data, key []byte) ([]byte, error) {
 	return plaintext, nil
 }
 
-func generateKey() ([]byte, error) {
-	key := make([]byte, 32)
+// generateKey is used by the module's encryption functions to create
+// a random key of the provided size.
+func generateKey(size int) ([]byte, error) {
+	key := make([]byte, size)
 	for i := range key {
 		key[i] = byte(rand.Intn(256))
 	}
 	return key, nil
 }
 
-func Unzip(zipData []byte) error {
-	zipReader, err := zip.NewReader(bytes.NewReader(zipData), int64(len(zipData)))
+// Unzip will extract files from the provided zip data. If no path is
+// provided, it will default to Pwd().
+func Unzip(zipData []byte, path ...string) error {
+	location := Pwd()
+	if len(path) > 0 {
+		location = path[0]
+	}
+
+	Sayf("Extracting zip contents to %s", location)
+
+	zipReader, err := zip.NewReader(
+		bytes.NewReader(zipData),
+		int64(len(zipData)),
+	)
 	if err != nil {
 		return err
 	}
 
 	for _, file := range zipReader.File {
-		filePath := filepath.Join(".", file.Name)
+		filePath := filepath.Join(location, file.Name)
 
 		if file.FileInfo().IsDir() {
+			Sayf("Creating %s%c", filePath, filepath.Separator)
 			os.MkdirAll(filePath, os.ModePerm)
 			continue
 		}
+
+		if dir := filepath.Dir(filePath); !Exists(dir) {
+			Sayf("Creating %s%c", dir, filepath.Separator)
+			os.MkdirAll(dir, os.ModePerm)
+		}
+
+		Sayf("Extracting %s", filePath)
 
 		fileData, err := file.Open()
 		if err != nil {
@@ -316,6 +532,8 @@ func Unzip(zipData []byte) error {
 			return err
 		}
 	}
+
+	Say("Finished extracting")
 
 	return nil
 }
